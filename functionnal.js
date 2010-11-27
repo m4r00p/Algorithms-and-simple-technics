01c69fcfd8207ac7d93a94ebe7165ee65dc03c54
Marek Pawłowski <marekapawlowski@gmail.com>
2010-11-27 20:08:26 +0100

    Some functional stuff. (bind/saturate/curry)

---
+++ functionnal.js
@@ -0,0 +1,46 @@
+
+// bind context and args to funciton 
+Function.prototype.bind = function (ctx /* ... */) {
+    var that = this;
+    var args = Array.prototype.slice.call(arguments, 1);
+
+    var fn = function () {
+        return that.apply(ctx, args.concat(Array.prototype.slice.call(arguments)));
+    };
+
+    return fn;
+};
+
+
+
+// saturate funciton
+Function.prototype.saturate = function () {
+    var that = this;
+    var args = Array.prototype.slice.call(arguments);
+
+    var fn = function () {
+        return that.apply(null, args);
+    };
+
+    return fn;
+};
+
+
+// curry arguments 
+Function.prototype.curry = function () {
+    var that = this;
+    var args = Array.prototype.slice.call(arguments);
+
+    var fn = function () {
+        return that.apply(null, args.concat(Array.prototype.slice.call(arguments)));
+    };
+
+    return fn;
+};
+
+var a = function (b, c) {
+   print(b + ' ' + c); 
+};
+
+//combo
+a.bind(null, "Wawa to").curry("Raga muffin").saturate()();
